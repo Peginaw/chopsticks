e4712c9c2bf71d988cc779acb7e8bdc4471c0b7f
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-06-14 15:47:02 -0400

    wrong branch

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,20 +4,21 @@ import { useState } from 'react';
 
 import Arena from './components/Arena';
 
-
+////////////////////////////////////////
+//////////////////// TO DO//////////////
+//1. game logic
+//2. scoreboard
+//////////////////////////////////////////
+/////////////////////////////////////////
 
 function App() {
   
 
-
-
   return (
     <div className="App">
-      <header className="App-header">
+  
        <Arena />
-       
-        
-      </header>
+      
     </div>
   );
 }
